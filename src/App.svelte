0f6fd99c573aa70ceb65ceeacceb1288309b3521
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-19 22:26:55 -0500

    First commit, working on login component

---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Login from './components/Login.svelte'
+	let loggedIN = false;
+	let userInfo = [];
+</script>
+
+<main>
+	<h1 class="logo"><span>dev</span>Nursery <span>B</span>logging</h1>
+	<Login/>
+</main>
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Major+Mono+Display&display=swap');
+
+main {padding: 100px;}
+
+.logo {text-align: center; font-size: 300%; width: 100%; margin: 10px auto; font-family: 'Major Mono Display', monospace;}
+
+.logo span {color: #A5243D;}
+</style>
---
+++ src/components/Login.svelte
@@ -0,0 +1,36 @@
+<script>
+export let loginStatus
+</script>
+
+<div class="login">
+
+<h2>Login</h2>
+
+<form>
+    <h3>username</h3>
+    <input type="text" name="username"/>
+    <h3>password</h3>
+    <input type="password" name="password"/>
+    <input type="submit" value="Login"/>
+</form>
+
+{#if userInfo}
+
+<button>Create Account</button>
+
+
+
+</div>
+
+
+<style>
+
+@import url('https://fonts.googleapis.com/css?family=Manjari|Staatliches&display=swap');
+
+.login {width: 300px; height: 300px; margin: 30px auto; background-color: #B9CFDF; text-align: center; padding: 10px; font-family: 'Manjari', sans-serif;}
+
+input { width: 60%; border: none; background-color: #A5243D; color: white; padding: 10px; margin: 5px;}
+
+h1,h2,h3,h4,h5,h6 {font-family: 'Staatliches', cursive;}
+
+</style>
